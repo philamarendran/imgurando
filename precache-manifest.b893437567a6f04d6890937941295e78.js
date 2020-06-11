a110d0a79261877b2f5077c0bce173c9cce55791
philamarendran <philipamarendran@gmail.com>
2020-06-11 15:48:29 -0400

    Updates

---
+++ precache-manifest.b893437567a6f04d6890937941295e78.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "644c2b92e31e6821e4ee318278704bea",
+    "url": "/imgurando/index.html"
+  },
+  {
+    "revision": "48e325f7fa6ebc76d54f",
+    "url": "/imgurando/static/css/main.3856c994.chunk.css"
+  },
+  {
+    "revision": "740f5c9d35eed84ac667",
+    "url": "/imgurando/static/js/2.be17bd1a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/imgurando/static/js/2.be17bd1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48e325f7fa6ebc76d54f",
+    "url": "/imgurando/static/js/main.fe926a89.chunk.js"
+  },
+  {
+    "revision": "778840542db38aad15c7",
+    "url": "/imgurando/static/js/runtime-main.4aa6e4f4.js"
+  },
+  {
+    "revision": "4b1f0b7a556ada6bcff707204c608ee3",
+    "url": "/imgurando/static/media/avenir_next_medium.4b1f0b7a.ttf"
+  }
+]);
